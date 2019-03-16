4cf62c9bab0d902c712534898b33468e61d8c774
k1n0F <35903605+k1n0F@users.noreply.github.com>
2019-03-16 18:27:05 +0200

    Update autoSeeker.sh

--- autoSeeker.sh
+++ autoSeeker.sh
@@ -10,7 +10,7 @@ pkg install git
 echo -e "\e[34mPart 1 done\e[0m"
 
 git clone https://github.com/thewhiteh4t/seeker
-cd seeker/termux
+cd $HOME/seeker/termux
 ./install.py
 
 echo -e "\e[1;33;4;44mТеперь пиши python2 seeker.py\e[0m"
